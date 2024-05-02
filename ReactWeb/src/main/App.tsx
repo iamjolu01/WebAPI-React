3d335213e017d21abf23dba4cd096c37c95b58c7
Hulozalem <iamjolu01@gmail.com>
2024-05-02 20:26:19 +0800

    added simple react

---
+++ ReactWeb/src/main/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle='Providing house all over the world!' />
+    </div>
+  );
+}
+
+export default App
